e331da1c6b7032c54484b520a6f201a8c4bd5edf
briana <52931537+brianapresley@users.noreply.github.com>
2019-11-09 18:16:47 -0500

    Add project files.

---
+++ StudentRegistration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StudentRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Student Registration App! \n");
+
+            Console.Write("Enter First Name: ");
+
+            string fName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+
+            string lName = Console.ReadLine();
+
+            Console.Write("What year were you born? ");
+
+            string bYear = Console.ReadLine();
+
+            Console.WriteLine($"\nWelcome {fName} {lName}!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine($"Your temporary password is: {fName}*{bYear}");
+        }   
+    }
+}
